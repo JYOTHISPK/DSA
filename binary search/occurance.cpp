2751c00e9cfce5fd90a0ee528411c8cfde4c1bcc
JYOTHIS <jyothisf15@gmail.com>
2025-07-29 22:26:41 +0530

    add 2 dsa files using binary search

---
+++ binary search/occurance.cpp
@@ -0,0 +1,58 @@
+/*
+--------------------------------------------
+Problem     : Find First And Last Occurance Of An Element
+              In A Sorted Array.
+Category    : Binary Search
+Language    : C++
+Author      : Jyothis P K
+Date        : 29-07-2025
+Description : Given an array of integers arr which is sorted in
+              ascending order,and an integer target, write a function  
+              to find first and last occurance of target in the array.
+
+Time Complexity : 2 x o(log n)
+Space Complexity: O(1) 
+--------------------------------------------
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int lower(int arr[],int start,int end,int target){
+    int ans=end+1;
+    while(start<=end){
+        int mid=(start+end)/2;
+        if(arr[mid]>=target)    ans=mid,end=mid-1;
+        else    start=mid+1;
+    }
+    return ans;
+}
+
+int upper(int arr[],int start,int end,int target){
+    int ans=end+1;
+    while(start<=end){
+        int mid=(start+end)/2;
+        if(arr[mid]>target)    ans=mid,end=mid-1;
+        else    start=mid+1;
+    }
+    return ans;
+}
+
+int main(){
+    int n,target;
+    cout << "enter size of array : ";
+    cin >> n;
+    int arr[n];
+    cout << "enter elements : ";
+    for(int i=0;i<n;i++)    cin >> arr[i];
+    cout << "enter target : ";
+    cin >> target;
+    int ans=lower(arr,0,n-1,target);
+    int ans1=upper(arr,0,n-1,target);
+    if(ans==n || arr[ans]!=target){
+        cout << "first and last occurance is (-1,-1)";
+    }
+    else{
+        cout << "first and last occurance is " << ans << " , " << ans1-1;
+    }
+}
